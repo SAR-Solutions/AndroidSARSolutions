a2d9654ada8ddfb4f1b35c65cffa646c68dd27fb
kunaalkumar <kunaal@ashiana.com>
2019-10-06 03:38:48 -0600

    Implement guards for activity state changes Implement firebase checks for the future Add missing drawables

--- app/src/main/java/com/example/sarsolutions/DTO.kt
+++ app/src/main/java/com/example/sarsolutions/DTO.kt
@@ -1,7 +1,6 @@
 package com.example.sarsolutions
 
+import com.google.firebase.firestore.GeoPoint
 import java.util.*
 
-data class GeoLocation(val lat: String, val long: String)
-
-data class Shift(val CaseID: String, val UserID: String, val StartTime: String, val locations: List<GeoLocation>)
+data class Shift(val caseId: String, val userId: String, val startTime: String, val path: List<GeoPoint>)
--- app/src/main/java/com/example/sarsolutions/MainActivity.kt
+++ app/src/main/java/com/example/sarsolutions/MainActivity.kt
@@ -36,14 +36,18 @@ import com.karumi.dexter.Dexter
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.firestore.SetOptions
 import com.karumi.dexter.listener.PermissionRequest
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
+import kotlin.collections.ArrayList
 
 @Suppress("LABEL_NAME_CLASH")
 class MainActivity : AppCompatActivity() {
@@ -51,8 +55,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
-    private val fireBaseService = RetrofitFactory.makeFirebaseRetrofitService()
+    private val locationList : ArrayList<GeoPoint> = ArrayList<GeoPoint>()
     private var isSearching = false
+    private var currentShiftId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,48 +65,54 @@ class MainActivity : AppCompatActivity() {
 
         val db = FirebaseFirestore.getInstance()
 
-       db.collection("Shift")
-           .get()
-           .addOnSuccessListener { result ->
-               for (document in result) {
-                   Toast.makeText(this, "${document.id} ==> ${document.data}", Toast.LENGTH_LONG).show()
-               }
-           }
-
-        start_button.setOnClickListener {
-            if(!isSearching) {
-                isSearching = true
-                start_button.text = "Stop"
-            } else {
-                isSearching = false
-                start_button.text = "Start"
-            }
-            val geoLocations = listOf(GeoLocation("123", "321"), GeoLocation("12", "420"))
-            val shift = Shift("YlNtlx3VTh6rAv6KC9dU", "oKrbMcbPVJ6yiErezkX3", Calendar.getInstance().time.toString(), geoLocations)
-            db.collection("Shift")
-                .add(shift)
-                .addOnSuccessListener { docRef ->
-                    Log.d("SAR", "Added document with id ${docRef.id}")
-                }
-        }
-
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         createLocationRequest()
         Dexter.withActivity(this)
             .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
             .withListener(object : PermissionListener {
-                override fun onPermissionGranted(response: PermissionGrantedResponse) {/* ... */
-                    locStuff();
+                override fun onPermissionGranted(response: PermissionGrantedResponse) {
+
+                    start_button.setOnClickListener {
+                        // Change button state/functionality
+                        if(!isSearching) { // Start searching
+                            isSearching = true
+                            start_button.text = "Stop"
+                            locStuff()
+                        } else { // Stop searching and update Loc
+                            fusedLocationClient.removeLocationUpdates(locationCallback)
+                            // TODO: Stop handler from updating
+                            val shift = Shift("YlNtlx3VTh6rAv6KC9dU", "oKrbMcbPVJ6yiErezkX3", Calendar.getInstance().time.toString(), locationList)
+
+                            /** NOTE: currentShiftId in this context is always going to be null for the demo. This code is here for the future **/
+
+                            if(currentShiftId == null) { // Add if no current shift id exists
+                                db.collection("Shift")
+                                    .add(shift)
+                                    .addOnSuccessListener { docRef ->
+                                        Log.d("SAR", "Added document with id ${docRef.id}")
+                                    }
+                            }
+                            else { // Update and merge if current shift id exists
+                                db.collection("Shift")
+                                    .document(currentShiftId!!)
+                                    .set(shift, SetOptions.merge())
+                            }
+                            isSearching = false
+                            start_button.text = "Start"
+                            currentShiftId = null
+                        }
+                    }
                 }
 
-                override fun onPermissionDenied(response: PermissionDeniedResponse) {/* ... */
+                override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                    // TODO: Add permissions check
                 }
 
                 override fun onPermissionRationaleShouldBeShown(
                     permission: PermissionRequest,
                     token: PermissionToken
-                ) {/* ... */
+                ) {
                 }
             }).check()
     }
@@ -116,9 +127,10 @@ class MainActivity : AppCompatActivity() {
                 super.onLocationResult(locationResult)
                 locationResult ?: return
                 for (location in locationResult.locations){
+
                     // Update UI with location data
-                    // ...
-                    location_id.text = location.longitude.toString()
+                    locationList.add(GeoPoint(location.latitude, location.longitude))
+                    location_id.text = "Last updated at \n" + Calendar.getInstance().time.toString()
                 }
             }
         }
@@ -132,11 +144,26 @@ class MainActivity : AppCompatActivity() {
         locationRequest = LocationRequest.create().apply {
             interval = 5000
             fastestInterval = 5000
-        }
+        } // update every 5 seconds
     }
 
     override fun onStop() {
         super.onStop()
-        fusedLocationClient.removeLocationUpdates(locationCallback)
+        if(isSearching) {
+            // TODO: Update fields
+            fusedLocationClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    // Runs every 10 minutes to sync locations with the database
+    private fun databaseSyncer() {
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            TODO("Sync locations with database")
+        }, 100000)
     }
 }
