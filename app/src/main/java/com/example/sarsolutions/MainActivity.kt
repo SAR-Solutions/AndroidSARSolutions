946bbce3195ce31873a97719393eda5bed75e1bf
kunaalkumar <kunaal@ashiana.com>
2019-12-17 16:53:40 -0700

    Improve poor navbar handling

--- app/src/main/java/com/example/sarsolutions/MainActivity.kt
+++ app/src/main/java/com/example/sarsolutions/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.sarsolutions
 
 import android.Manifest
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -53,27 +52,21 @@ class MainActivity : AppCompatActivity() {
                 }
             }).check()
 
-        if (savedInstanceState == null) {
-            // Starting off in login fragment where appbar needs to be hidden
-            appbar.visibility = View.INVISIBLE
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Set loginFragment and caseFragment as top-level destinations to prevent showing back
+        //  on these screens
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.loginFragment, R.id.casesFragment))
 
+        appbar.setupWithNavController(navController, appBarConfiguration)
+
+        if (savedInstanceState == null) {
             val auth = FirebaseAuth.getInstance()
-            val navController = findNavController(R.id.nav_host_fragment)
             val graph = navController.graph
 
             // If user is logged in, navigate to case list fragment
             if (auth.currentUser != null) {
                 graph.startDestination = R.id.casesFragment
                 navController.graph = graph
-            }
-            appbar.setupWithNavController(navController, AppBarConfiguration(navController.graph))
-
-
-            findNavController(R.id.nav_host_fragment).addOnDestinationChangedListener { controller, destination, arguments ->
-                if (destination.id == R.id.casesFragment)
-                    appbar.visibility = View.VISIBLE
-                else if (destination.id == R.id.loginFragment)
-                    appbar.visibility = View.INVISIBLE
             }
         }
     }
