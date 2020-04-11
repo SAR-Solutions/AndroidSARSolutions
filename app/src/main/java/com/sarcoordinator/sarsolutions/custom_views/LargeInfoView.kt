04627c829da81f2f10b3d6e934f1ad9780efb2c1
kunaalkumar <kunaal@ashiana.com>
2020-04-11 12:20:19 -0700

    Fix message text view alignment in large_info_view.xml Add animation in LargeInfoView.kt

--- app/src/main/java/com/sarcoordinator/sarsolutions/custom_views/LargeInfoView.kt
+++ app/src/main/java/com/sarcoordinator/sarsolutions/custom_views/LargeInfoView.kt
@@ -1,7 +1,11 @@
 package com.sarcoordinator.sarsolutions.custom_views
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import android.widget.LinearLayout
 import com.sarcoordinator.sarsolutions.R
 import kotlinx.android.synthetic.main.large_info_view.view.*
@@ -28,6 +32,42 @@ class LargeInfoView(context: Context, attrs: AttributeSet) : LinearLayout(contex
             } finally {
                 recycle()
             }
+        }
+
+        // Play animation after layout is inflated
+        viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                startAnim()
+                viewTreeObserver.removeOnGlobalLayoutListener(this)
+            }
+        })
+    }
+
+    private fun startAnim() {
+        val imgCenter = (icon.y + icon.height) / 2
+        val headingCenter = (heading.y + heading.height) / 2
+
+        val iconAlphaAnim = ObjectAnimator.ofFloat(icon, View.ALPHA, icon.alpha, 1F)
+        val headingAlphaAnim = ObjectAnimator.ofFloat(heading, View.ALPHA, heading.alpha, 1F)
+        val headingDropAnim = ObjectAnimator.ofFloat(heading, View.Y, imgCenter, heading.y)
+        val messageAlphaAnim = ObjectAnimator.ofFloat(message, View.ALPHA, message.alpha, 1F)
+        val messageDropAnim = ObjectAnimator.ofFloat(message, View.Y, headingCenter, message.y)
+
+        val messageAnim = AnimatorSet().apply {
+            play(messageAlphaAnim)
+                .with(messageDropAnim)
+        }
+
+        val headingAnim = AnimatorSet().apply {
+            play(headingAlphaAnim)
+                .with(headingDropAnim)
+                .with(messageAnim)
+        }
+
+        AnimatorSet().apply {
+            play(headingAnim)
+                .after(iconAlphaAnim)
+            start()
         }
     }
 }
