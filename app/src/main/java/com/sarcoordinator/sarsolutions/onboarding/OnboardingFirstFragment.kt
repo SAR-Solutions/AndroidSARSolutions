444b802ecb5a2e6f574ee1784b275d84ea023187
kunaalkumar <kunaal@ashiana.com>
2020-04-17 19:46:28 -0700

    Add onboarding fragments Add images for onboarding fragments Add catamaran font Add viewpager 2 dependency

---
+++ app/src/main/java/com/sarcoordinator/sarsolutions/onboarding/OnboardingFirstFragment.kt
@@ -0,0 +1,6 @@
+package com.sarcoordinator.sarsolutions.onboarding
+
+import androidx.fragment.app.Fragment
+import com.sarcoordinator.sarsolutions.R
+
+class OnboardingFirstFragment : Fragment(R.layout.fragment_onboarding_first)
---
+++ app/src/main/java/com/sarcoordinator/sarsolutions/onboarding/OnboardingThirdFragment.kt
@@ -0,0 +1,6 @@
+package com.sarcoordinator.sarsolutions.onboarding
+
+import androidx.fragment.app.Fragment
+import com.sarcoordinator.sarsolutions.R
+
+class OnboardingThirdFragment : Fragment(R.layout.fragment_onboarding_third)
