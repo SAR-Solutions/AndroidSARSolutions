63f395914c4addccc52a8c561151629db5e7f77e
kunaalkumar <kunaal@ashiana.com>
2020-04-18 22:39:32 -0700

    Implement saving onboarding viewpager state on configuration change

--- app/src/main/java/com/sarcoordinator/sarsolutions/MainActivity.kt
+++ app/src/main/java/com/sarcoordinator/sarsolutions/MainActivity.kt
@@ -63,6 +63,13 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
         if (savedInstanceState != null) {
 
+            // Only restore navigation component state if user is logged in
+            // If user isn't logged in, navigation component doesn't handle navigation
+            if (auth.currentUser == null) {
+                hideBottomNavBar(true)
+                return
+            }
+
             // Recover Navigation state from process death
             savedInstanceState.getSerializable(BACKSTACK)?.let {
                 nav.setBackStack(it as HashMap<*, Collection<String>>)
@@ -109,6 +116,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+
         outState.putSerializable(BACKSTACK, nav.getBackStack())
         outState.putSerializable(TABSTACK, ArrayList(nav.getTabStack().toList()))
         outState.putSerializable(FRAGMENT_STATES_MAP, nav.getFragmentStateMap())
--- app/src/main/java/com/sarcoordinator/sarsolutions/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/sarcoordinator/sarsolutions/onboarding/OnboardingFragment.kt
@@ -19,9 +19,21 @@ import kotlinx.android.synthetic.main.fragment_onboarding.*
 
 class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
 
+    private lateinit var adapter: OnboardingAdapter
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("Current", onboarding_view_pager.currentItem)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = OnboardingAdapter(this)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        onboarding_view_pager.adapter = OnboardingAdapter(this)
+        onboarding_view_pager.adapter = adapter
 
         setupOnboardingIndicators()
 
@@ -57,6 +69,10 @@ class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
                 }
             }
         })
+
+        savedInstanceState?.let {
+            onboarding_view_pager.setCurrentItem(savedInstanceState.getInt("Current"), false)
+        }
     }
 
     override fun onStart() {
