bfd2a09b667b4bc31213ccc3b0b798bb8d906905
kunaalkumar <kunaal@ashiana.com>
2020-04-22 15:27:20 -0700

    Fix missing "try again" network button on cases screen Add button to large_info_view.xml Add conditional button animation and visibility to large_info_view.xml Enable setting click listener on button in large_info_view.xml

--- app/src/main/java/com/sarcoordinator/sarsolutions/custom_views/LargeInfoView.kt
+++ app/src/main/java/com/sarcoordinator/sarsolutions/custom_views/LargeInfoView.kt
@@ -12,6 +12,8 @@ import kotlinx.android.synthetic.main.large_info_view.view.*
 
 class LargeInfoView(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
 
+    private var enableButton = false
+
     init {
         context.theme.obtainStyledAttributes(
             attrs,
@@ -44,21 +46,30 @@ class LargeInfoView(context: Context, attrs: AttributeSet) : LinearLayout(contex
         }
     }
 
+    fun setButtonClickListener(clickListener: OnClickListener) {
+        enableButton = true
+        button.setOnClickListener(clickListener)
+    }
+
     private fun startAnim() {
 
         // Reset view alpha
         icon.alpha = 0F
         heading.alpha = 0F
         message.alpha = 0F
+        button.alpha = 0F
 
         val imgCenter = (icon.y + icon.height) / 2
         val headingCenter = (heading.y + heading.height) / 2
+        val messageCenter = (message.y + message.height) / 2
 
         // Skip animation as view is not inflated yet
-        if (imgCenter == 0F || headingCenter == 0F) {
+        if (imgCenter == 0F || headingCenter == 0F || messageCenter == 0F) {
             icon.alpha = 1F
             heading.alpha = 1F
             message.alpha = 1F
+            if (enableButton)
+                button.alpha = 1F
             return
         }
 
@@ -67,6 +78,13 @@ class LargeInfoView(context: Context, attrs: AttributeSet) : LinearLayout(contex
         val headingDropAnim = ObjectAnimator.ofFloat(heading, View.Y, imgCenter, heading.y)
         val messageAlphaAnim = ObjectAnimator.ofFloat(message, View.ALPHA, message.alpha, 1F)
         val messageDropAnim = ObjectAnimator.ofFloat(message, View.Y, headingCenter, message.y)
+        val buttonAlphaAnim = ObjectAnimator.ofFloat(button, View.ALPHA, button.alpha, 1F)
+        val buttonDropAnim = ObjectAnimator.ofFloat(button, View.Y, messageCenter, button.y)
+
+        val buttonAnim = AnimatorSet().apply {
+            play(buttonAlphaAnim)
+                .with(buttonDropAnim)
+        }
 
         val messageAnim = AnimatorSet().apply {
             play(messageAlphaAnim)
@@ -74,9 +92,12 @@ class LargeInfoView(context: Context, attrs: AttributeSet) : LinearLayout(contex
         }
 
         val headingAnim = AnimatorSet().apply {
-            play(headingAlphaAnim)
+            val anim = play(headingAlphaAnim)
                 .with(headingDropAnim)
                 .with(messageAnim)
+
+            if (enableButton)
+                anim.with(buttonAnim)
         }
 
         AnimatorSet().apply {
